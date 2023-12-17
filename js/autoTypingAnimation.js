9a0763f0e2ecfdf5f2a87409cd6e6fa9000ec872
kartikkrishnasharmaa <kartikkrishnasharmaa@gmail.com>
2023-12-17 12:51:41 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout( function () {
+  $( '.about-info-inner' ).typed( {
+    strings: [
+      "<span>Hi, I am Kartik Sharma</span> Highly motivated and detail-oriented Data Analyst and Web developer with a strong foundation in data analysis, visualization tools, and web development.Skilled in developing and implementing data collection systems, identifying business needs."
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  } );
+}, 100 );
